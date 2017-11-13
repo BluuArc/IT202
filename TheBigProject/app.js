21432c3b6e9aa84f2d4f672d6eacfe845c82b141
Joshua Castor <jcasto3@uic.edu>
2017-11-13 15:47:25 +0000

    Added map page to the big project

--- TheBigProject/app.js
+++ TheBigProject/app.js
@@ -9,7 +9,9 @@ var App = function(options){
             },
             "#mapPage": {
                 name: "Map",
-                drawer: true
+                drawer: true,
+                map: undefined,
+                currentLocationMarker: undefined
             },
             "#markerListPage": {
                 name: "Marker List",
@@ -50,6 +52,46 @@ var App = function(options){
         
     }
     
+    //from app14
+    function getCurrentLocation() {
+        return new Promise((fulfill,reject) => {
+            if (!navigator.geolocation){
+                alert("Geolocation is not supported by your browser");
+                reject("Geolocation is not supported by your browser");
+            }
+            navigator.geolocation.getCurrentPosition(fulfill, reject);
+        });
+    }
+        
+        // from https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
+        function showCurrentLocation(mapData){
+            return getCurrentLocation()
+                .then((data) => { //show marker
+                    //hide previous marker
+                    if(mapData.currentLocationMarker){
+                        // from https://developers.google.com/maps/documentation/javascript/markers
+                        mapData.currentLocationMarker.setMap(null);
+                    }
+                    
+                    
+                    let coords = {
+                        lat: data.coords.latitude,
+                        lng: data.coords.longitude
+                    }
+                    
+                    mapData.currentLocationMarker = new google.maps.Marker({
+                      position: coords,
+                      map: mapData.map
+                    });
+                    
+                    mapData.map.setCenter(coords);
+                    
+                    return;
+                }).catch((err) => {
+                    console.error((err));
+                })
+        }
+    
     
     function init() {
         mdc.autoInit();
@@ -58,6 +100,11 @@ var App = function(options){
         document.querySelector('.menu').addEventListener('click', () => drawer.open = !drawer.open);
         
         //intialize pages
+        let pages = $(".pages");
+        
+        pages.css("padding-top", $("#mainNavbar").height()*1.1);
+        $("#mapPage #map").css("height", $("body").height() - $("#mainNavbar").height())
+            .css("margin-top", '14px');
         self.navbar.title = $(".mdc-toolbar__title#navbarTitle");
         $.each(Object.keys(self.pages), function(i,d) {
             $("a" + d).on("click", function (e) {
@@ -65,6 +112,14 @@ var App = function(options){
                 setPageTo(d);
             });
         });
+        
+        // initialize map
+        self.pages["#mapPage"].map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+        });
+        
+        
+        return showCurrentLocation(self.pages["#mapPage"]);
     }
     
     
